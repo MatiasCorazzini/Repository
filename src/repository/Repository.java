1675095d62fe4fae6b486ae60942feadc9ea17d8
MatiasCorazzin <matiascorazzini@hotmail.com>
2021-06-15 13:32:06 -0300

    MiCommit01

---
+++ src/repository/Repository.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package repository;
+
+/**
+ *
+ * @author matia
+ */
+public class Repository {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello Repository");
+    }
+    
+}
